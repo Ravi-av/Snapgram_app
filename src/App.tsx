81d6dbe443a72cc317fcb86eb7cf8bc6faa125a5
ravi-av <ravisrivastav2609@gmail.com>
2024-02-22 02:34:17 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 
-// import {
-//   Home,
-//   Explore,
-//   Saved,
-//   CreatePost,
-//   Profile,
-//   EditPost,
-//   PostDetails,
-//   UpdateProfile,
-//   AllUsers,
-// } 
-// from "./_root/pages";
 import AuthLayout from "./_auth/AuthLayout";
 // import RootLayout from "./_root/RootLayout   ";
 import SignupForm from "./_auth/forms/SignupForm";
 import SigninForm from "./_auth/forms/SigninForm";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
-
-
-
-import { Home } from "./_root/pages";
 import RootLayout from "./_root/RootLayout";
+
+
+ 
+import AllUsers from "./_root/pages/AllUsers";
+import PostDetails from "./_root/pages/PostDetails";
+import UpdateProfile from "./_root/pages/UpdateProfile";
+import { CreatePost, EditPost, Explore, Home, Profile, Saved } from "./_root/pages";
 
 const App = () => {
   return (
@@ -37,14 +28,14 @@ const App = () => {
         {/* private routes */}
         <Route element={<RootLayout />}>
           <Route index element={<Home/>} />
-          {/* <Route path="/explore" element={<Explore />} />
+          <Route path="/explore" element={<Explore />} />
           <Route path="/saved" element={<Saved />} />
           <Route path="/all-users" element={<AllUsers />} />
-          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/Create-post" element={<CreatePost />} />
           <Route path="/update-post/:id" element={<EditPost />} />
-          <Route path="/posts/:id" element={<PostDetails />} /> */}
-          {/* <Route path="/profile/:id/*" element={<Profile />} /> */}
-          {/* <Route path="/update-profile/:id" element={<UpdateProfile />} /> */}
+          <Route path="/posts/:id" element={<PostDetails />} /> 
+          <Route path="/profile/:id/*" element={<Profile />} /> 
+          <Route path="/update-profile/:id" element={<UpdateProfile />} />
         </Route>
       </Routes>
 
